f5b9d8691bca672cd99a9b8f9a7e8251fa08c990
Madison Gallagher <maga0020@colorado.edu>
2021-12-11 23:32:31 -0700

    MADWORN

--- MADWORN/index.js
+++ MADWORN/index.js
@@ -1,7 +1,11 @@
+//
+// document.getElementByClassName("container1")[i].addEventListener("click", function(){changeColor(this.id)});
+//
+// function changeColor(sectionID) {
+//   console.log(sectionID)
+//   document.getElementById("demo").classList.toggle("red");
+// }
 
-document.getElementByClassName("container")[i].addEventListener("click", function(){changeColor(this.id)});
-
-function changeColor(sectionID) {
-  console.log(sectionID)
-  document.getElementById("demo").classList.toggle("red");
+function myFunction(x) {
+  x.classList.toggle("fa-thumbs-down");
 }
